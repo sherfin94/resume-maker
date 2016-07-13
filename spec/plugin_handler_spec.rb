f51b6049519815bc83a2d371e54827c4a5756c28
Sherfin S <sherfin94@gmail.com>
2016-07-13 17:45:19 +0530

    remove conflicting plugin load test

--- spec/plugin_handler_spec.rb
+++ spec/plugin_handler_spec.rb
@@ -1,1 +1,26 @@
+require 'plugin_handler'
 
+describe PluginHandler do
+  let(:plugin_handler) { PluginHandler.new }
+  describe '#load_plugins' do
+    it 'does not leave the plugin set empty' do
+      plugin_handler.load_plugins
+      expect(plugin_handler.list_plugins.length).not_to eq(0)
+    end
+  end
+  it 'responds to export' do
+    expect(plugin_handler).to respond_to(:export)
+  end
+  it 'responds to list_plugins with an array' do
+    expect(plugin_handler.list_plugins).to be_a(Array)
+  end
+  it 'responds to current_format=' do
+    expect(plugin_handler).to respond_to(:current_format=)
+  end
+  it 'responds to load_plugins' do
+    expect(plugin_handler).to respond_to(:load_plugins)
+  end
+  it 'responds to load_classes_from_folder' do
+    expect(plugin_handler).to respond_to(:load_classes_from_folder)
+  end
+end
