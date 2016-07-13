2d5523de502b4ee2739e9df4fcd56f1a37a88599
Sherfin S <sherfin94@gmail.com>
2016-07-13 18:56:45 +0530

    remove reduntant expects

--- spec/resume_maker_spec.rb
+++ spec/resume_maker_spec.rb
@@ -3,32 +3,26 @@ require 'resume_maker'
 describe 'ResumeMaker' do
   let(:maker) { ResumeMaker.new }
 
+
   it 'responds to the start call that starts the program' do
     expect(maker).to respond_to(:start)
   end
 
-  it 'can set a user_io_handler' do
+  it 'sets a user_io_handler' do
     expect(maker).to respond_to(:io_handler=).with(1)
   end
 
-  it 'can set a plugin_handler' do
+  it 'sets a plugin_handler' do
     expect(maker).to respond_to(:plugin_handler=).with(1)
   end
 
   describe '#display_main_menu_and_get_choice' do
     it 'gets a user choice from the list of items supplied' do
       io_handler = instance_double('UserIOHandler')
-      allow(io_handler).to receive(:display_menu_and_get_choice).with(
-        kind_of(Array),
-        kind_of(String)
-      ).and_return(1, 2, 3)
+      allow(io_handler).to receive(:display_menu_and_get_choice).and_return(1)
       maker.io_handler = io_handler
       expect(maker.display_main_menu_and_get_choice)
         .to eq(1)
-      expect(maker.display_main_menu_and_get_choice)
-        .to eq(2)
-      expect(maker.display_main_menu_and_get_choice)
-        .to eq(3)
     end
   end
 
@@ -36,7 +30,7 @@ describe 'ResumeMaker' do
     it 'displays the available formats and gets the user choice' do
       io_handler = instance_double('UserIOHandler')
       allow(io_handler).to receive(:display_menu_and_get_choice)
-        .and_return(1, 2, 3)
+        .and_return(1)
       plugin_handler = instance_double('pluginLoader')
       allow(plugin_handler).to receive(:load_plugins)
       allow(plugin_handler).to receive(:list_plugins).and_return(
@@ -46,8 +40,6 @@ describe 'ResumeMaker' do
       maker.io_handler = io_handler
       maker.plugin_handler = plugin_handler
       expect(maker.display_format_choice_menu_and_get_choice).to eq(1)
-      expect(maker.display_format_choice_menu_and_get_choice).to eq(2)
-      expect(maker.display_format_choice_menu_and_get_choice).to eq(3)
     end
   end
 
