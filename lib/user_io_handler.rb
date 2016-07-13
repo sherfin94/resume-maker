ff3d1b69b456f5b2b5dbb84b233eeaeab96b9cad
Sherfin S <sherfin94@gmail.com>
2016-07-13 11:32:19 +0530

    add class UserIOHandler

---
+++ lib/user_io_handler.rb
@@ -0,0 +1,25 @@
+# Handles User Input Output
+class UserIOHandler
+  def show_message(message)
+    puts message
+  end
+
+  def display_menu_and_get_choice(list, required_choice_criteria)
+    puts "Choose your #{required_choice_criteria}\n"
+    list.each_with_index do |item, index|
+      puts "#{index + 1}. #{item}"
+    end
+
+    print 'your option : '
+    Integer(gets) + 1
+  end
+
+  def ask_user_for(parameter)
+    print "Enter #{parameter} : "
+    gets
+  end
+
+  def clear_screen
+    # system 'clear'
+  end
+end
