64fe367c20d1452def270cba20a52fc7bc6fbb22
Sherfin S <sherfin94@gmail.com>
2016-07-14 18:20:11 +0530

    modify tests to override standard IO to verify IOHandler
    STDIN and STDOUT are stubbed by using StringIO instances and their outputs are
    verified against expected output

--- spec/user_io_handler_spec.rb
+++ spec/user_io_handler_spec.rb
@@ -1,16 +1,42 @@
 require 'user_io_handler'
 
+def with_captured_stdout
+  begin
+    old_stdout = $stdout
+    $stdout = StringIO.new('','w')
+    yield
+    $stdout.string
+  ensure
+    $stdout = old_stdout
+  end
+end
+
 describe UserIOHandler do
   let(:io_handler) { UserIOHandler.new }
-  it 'responds to show_message' do
-    expect(io_handler).to respond_to(:show_message)
+  it 'shows requested message' do
+    output = with_captured_stdout do
+      io_handler.show_message('hello world')
+    end
+    expect(output).to eq("hello world\n")
   end
-  it 'responds to display_menu_and_get_choice with two arguements' do
-    expect(io_handler).to respond_to(:display_menu_and_get_choice).with(2)
+  it 'shows requested menu' do
+    # expect(io_handler).to respond_to(:display_menu_and_get_choice).with(2)
+    # allow(STDIN).to receive(:gets).and_return('1')
+    $stdin = StringIO.new('1')
+    output = with_captured_stdout do
+      io_handler.display_menu_and_get_choice(%(a b).split, 'letter')
+    end
+    expect(output).to eq("Choose your letter\n1. a\n2. b\nyour option : ")
   end
-  it 'responds to ask_user_for with one arguement' do
-    expect(io_handler).to respond_to(:ask_user_for).with(1)
+  it 'asks user for a parameter' do
+    # expect(io_handler).to respond_to(:ask_user_for).with(1)
+    $stdin = StringIO.new('myans')
+    output = with_captured_stdout do
+      io_handler.ask_user_for('your answer')
+    end
+    expect(output).to eq('Enter your answer : ')
   end
+
   it 'responds to clear screen' do
     expect(io_handler).to respond_to(:clear_screen)
   end
