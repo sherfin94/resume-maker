21fe3753e158440d097357a68362d074783db483
Sherfin S <sherfin94@gmail.com>
2016-07-14 16:19:27 +0530

    remove initialization test from user spec

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,9 +2,6 @@ require 'user'
 
 describe User do
   let(:user) { User.new('Some Name', '21', 'Some Place') }
-  it 'is initializable with three arguments' do
-    expect(User).to respond_to(:new).with(3)
-  end
   it 'responds to name and name=' do
     expect(user).to respond_to(:name)
     expect(user).to respond_to(:name=)
@@ -15,6 +12,6 @@ describe User do
   it 'resturns a hash with name, age, place when called to_hash' do
     the_hash = user.to_hash
     expect(the_hash).to be_a(Hash)
-    expect(the_hash.keys).to include(:name, :age, :place)
+    expect(the_hash).to eq({name: 'Some Name', age: '21', place: 'Some Place'})
   end
 end
